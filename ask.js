ab6158e369d84c3a92ff428f77e566c5b31e2e2e
chenguoqing <chenguoqing@zhuojianchina.com>
2021-01-28 20:14:05 +0800

    add: 新增复杂模板

---
+++ ask.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+      type: 'confirm',
+      name: 'private',
+      message: 'ths resgistery is private?',
+    },
+    {
+      type: 'input',
+      name: 'author',
+      message: 'author?',
+    },
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'please input projectName',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'description?',
+    }
+  ]
